b864fbdb60bf7ee3fe3a64fe948691efbb42b149
sundayjoa <jeonjoo1004@gmail.com>
2024-06-08 11:48:48 +0900

    깃허브 연동

---
+++ Diary_project/src/main/java/com/example/diary/DiaryProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//깃허브 업데이트 테스트
+@SpringBootApplication
+public class DiaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryProjectApplication.class, args);
+	}
+
+}
