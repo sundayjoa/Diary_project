d93cef4ed017588eca040316656daf11b54ad947
sundayjoa <jeonjoo1004@gmail.com>
2024-06-21 14:40:51 +0900

    회원가입 기능 완료

--- Diary_project/src/main/java/com/example/diary/controller/MembershipController.java
+++ Diary_project/src/main/java/com/example/diary/controller/MembershipController.java
@@ -3,6 +3,8 @@ package com.example.diary.controller;
 import com.example.diary.model.Membership;
 import com.example.diary.service.MembershipService;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,7 @@ public class MembershipController {
     private MembershipService membershipService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerMembership(@RequestBody Membership membership) {
+    public ResponseEntity<String> registerMembership(@RequestBody Membership membership, HttpSession session) {
     	
     	if (membership.getId() == null || membership.getId().isEmpty()) {
             return ResponseEntity.badRequest().body("ID는 필수 입력값입니다.");
@@ -28,6 +30,7 @@ public class MembershipController {
     	 try {
              membershipService.saveMembership(membership);
              logger.info("회원가입이 성공적으로 완료되었습니다: " + membership.getId());
+             session.setAttribute("userId", membership.getId());
              return ResponseEntity.ok("회원가입이 성공적으로 완료되었습니다.");
          } catch (Exception e) {
              logger.error("회원가입 중 오류가 발생했습니다.", e);
