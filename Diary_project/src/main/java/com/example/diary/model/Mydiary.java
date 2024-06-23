970efb9bbea582d37512c4b71c167f8f4f6ffe4b
sundayjoa <jeonjoo1004@gmail.com>
2024-06-23 17:03:43 +0900

    주제 추천 ui

--- Diary_project/src/main/java/com/example/diary/model/Mydiary.java
+++ Diary_project/src/main/java/com/example/diary/model/Mydiary.java
@@ -3,22 +3,66 @@ package com.example.diary.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "my")
 public class Mydiary {
 	@Id
-    @Column(length = 30, nullable = false)
+	@Column(name="DiaryNumber", columnDefinition = "BIGINT")
+    private BigInteger DiaryNumber;
+		
+    @Column(name="ID", length = 30, nullable = false)
     private String id;
 	
-	@Column(name = "Password", length = 50)
-    private String password;
+    @Lob
+	@Column(name = "content")
+    private String content;
+    
+    @Column(name = "date")
+    private LocalDateTime date;
+    
+    @Column(name="weather", length = 100)
+    private String weather;
+    
+    @Column(name = "`public`")
+    private boolean isPublic;
+    
+    @Column(name = "`like`")
+    private int likeCount;
 	
 	@Column(name = "Name", length = 50)
     private String name;
 	
-	public String getId() {
+	
+	public BigInteger getDiaryNumber() {
+        return DiaryNumber;
+    }
+
+    public void setDiaryNumber(BigInteger DiaryNumber) {
+        this.DiaryNumber = DiaryNumber;
+    }
+	
+	public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+	public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    
+    public String getId() {
         return id;
     }
 
@@ -26,12 +70,28 @@ public class Mydiary {
         this.id = id;
     }
     
-    public String getPassword() {
-        return password;
+	public String getWeather() {
+        return id;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+    
+    public boolean getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+    
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
     }
     
     public String getName() {
