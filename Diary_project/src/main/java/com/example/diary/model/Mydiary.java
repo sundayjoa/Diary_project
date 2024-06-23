07d7e9c08d86c70a928f92e39b27420608559cb8
sundayjoa <jeonjoo1004@gmail.com>
2024-06-23 15:40:34 +0900

    나의 일기장 ui

---
+++ Diary_project/src/main/java/com/example/diary/model/Mydiary.java
@@ -0,0 +1,44 @@
+package com.example.diary.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "my")
+public class Mydiary {
+	@Id
+    @Column(length = 30, nullable = false)
+    private String id;
+	
+	@Column(name = "Password", length = 50)
+    private String password;
+	
+	@Column(name = "Name", length = 50)
+    private String name;
+	
+	public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
